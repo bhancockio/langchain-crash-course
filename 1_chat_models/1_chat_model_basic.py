75bd6f1dd29031a2c79cebd67f7d23bc20c016e5
Brandon Hancock <brandon@brandonhancock.io>
2024-06-15 14:47:14 -0400

    Update docs in chat models

--- 1_chat_models/1_chat_model_basic.py
+++ 1_chat_models/1_chat_model_basic.py
@@ -1,3 +1,6 @@
+# Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/
+# OpenAI Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/openai/
+
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 
