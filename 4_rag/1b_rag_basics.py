e42cf87b4168031f6e8cdf3735317a076f946764
Elbeqqal <youssef.elbeqqal@wipo.int>
2024-08-05 15:36:26 +0100

    increase score_threshold

--- 4_rag/1b_rag_basics.py
+++ 4_rag/1b_rag_basics.py
@@ -20,7 +20,7 @@ query = "Who is Odysseus' wife?"
 # Retrieve relevant documents based on the query
 retriever = db.as_retriever(
     search_type="similarity_score_threshold",
-    search_kwargs={"k": 3, "score_threshold": 0.9},
+    search_kwargs={"k": 3, "score_threshold": 0.4},
 )
 relevant_docs = retriever.invoke(query)
 
